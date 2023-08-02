050a8f6b0294f1a296b6ee018c59ee9e66293737
Mohd Aadil Rana <epochbrokers2gmail.com>
2023-08-02 16:39:52 +0530

    useCallback function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import "./App.css"
-import Display from "./components/Display";
-import UseCallback from "./components/UseCallback";
-import UseEffect from "./components/UseEffect";
-import UseReducer from "./components/UseReducer";
-import UseState from "./components/UseState";
+import "./App.css";
+import BetaApp from "./beta/BetaApp";
+// import Display from "./components/Display";
+// import UseCallback from "./components/UseCallback";
+// import UseEffect from "./components/UseEffect";
+// import UseReducer from "./components/UseReducer";
+// import UseState from "./components/UseState";
 const App = () => {
-    return <div className="App">
-      <UseState />
-      <UseEffect />
-      <Display />
-      <UseCallback />
-      <UseReducer />
-    </div>;
+    return (
+        <div className="App">
+            {/* <div className="normal">
+                <UseState />
+                <UseEffect />
+                <Display />
+                <UseCallback />
+                <UseReducer />
+            </div> */}
+            <div className="beta">
+              <BetaApp />
+            </div>
+        </div>
+    );
 };
 
 export default App;
